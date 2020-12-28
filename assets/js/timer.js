e211b937597f8332fb8f324df31ae5abed934a0f
Seijuro1010 <seijuro.defensegrid@gmail.com>
2020-12-28 23:52:06 +0800

    Refactored Event Pages. Added Map Previews. Added Mortar Madness

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -23,13 +23,14 @@ function initializeClock(id, endtime) {
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = ('0' + t.days).slice(-2);
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
     if (t.total <= 0) {
       clearInterval(timeinterval);
+    }
+    else {
+      daysSpan.innerHTML = ('0' + t.days).slice(-2);
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
     }
   }
 
